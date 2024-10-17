db1231de516c5968f0cd90f4270ec8505ad373cf
langtonhugh <s.h.langton@amsterdamumc.nl>
2024-10-17 18:30:31 +0200

    random sample and waffle

--- scripts/explore.R
+++ scripts/explore.R
@@ -3,6 +3,7 @@ library(readr)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(waffle)
 
 # Load data.
 coffee_survey <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-14/coffee_survey.csv')
@@ -19,8 +20,8 @@ coffee_survey_long <- coffee_survey %>%
 # Counts.
 count(coffee_survey_long, dairy)
 
-# Create dairy flag.
-coffee_survey <- coffee_survey_long %>% 
+# Create dairy flag in the original data.
+coffee_survey_long <- coffee_survey_long %>% 
   mutate(dairy_flag = recode(dairy,
     `Almond milk`             = "Alternative",
     `Coffee creamer`          = "Dairy",
@@ -33,20 +34,59 @@ coffee_survey <- coffee_survey_long %>%
     `None`                    = "None (black)"
   ))
 
- # Check counts.
-coffee_survey %>% 
+# Final clean. Drop missing political affiliation and randomly select one 
+# of their milk preferences, for people that chose > 1. 
+set.seed(1612)
+
+political_long <- coffee_survey_long %>% 
   drop_na(political_affiliation) %>%
-  ggplot(data = ) +
+  group_by(submission_id) %>%
+  sample_n(size = 1) %>% 
+  ungroup()
+
+# Plot.
+ggplot(data = political_long) +
   geom_bar(mapping = aes(x = political_affiliation, fill = dairy_flag),
+           position = "fill",
            colour = "black") +
   theme_bw() +
   coord_flip() +
   scale_fill_brewer(palette = "Set2") +
+  scale_y_continuous(labels = scales::percent) +
   labs(x = NULL, y = NULL, fill = NULL,
-       title = "Does dairy preference vary by political affiliation?",
+       title = "Do republicans prefer 'real' milk in their coffee?",
        caption = "Data source: Great American Coffee Taste Test (2023)") +
   theme(legend.position = "bottom") 
 
 # Save.
 ggsave(filename = "visuals/political_milk_draft.png",
        height = 12, width = 16, unit = "cm", dpi = 300)
+
+# Handling for more specific preferences.
+# Final clean. Drop missing political affiliation and randomly select one 
+# of their milk preferences, for people that chose > 1. 
+waf_data <- coffee_survey_long %>%
+  group_by(political_affiliation, dairy) %>% 
+  summarize(n = n()) %>% 
+  ungroup() %>% 
+  drop_na(political_affiliation) %>% 
+  filter(dairy != "None") %>% 
+  group_by(political_affiliation) %>% 
+  mutate(props = round(100*n/sum(n), 2)) %>% 
+  ungroup()
+
+# Make waffle (draft).
+ggplot(data = waf_data,
+         mapping = aes(fill = dairy, values = props)) +
+  geom_waffle(flip = TRUE) +
+  facet_wrap(~political_affiliation, nrow = 2, scales = "free") +
+  scale_fill_brewer(palette = "Set3") +
+  theme_bw()
+
+  
+  
+  
+  
+  
+
+
